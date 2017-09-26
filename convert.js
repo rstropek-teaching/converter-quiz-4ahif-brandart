3ebdffe5480890501ca1e8985aa26ca871b6a965
brandart <31988348+brandart@users.noreply.github.com>
2017-09-26 14:11:23 +0200

    Add files via upload
    Here is my solution for this homework.
    
    Arthur Brandstetter

--- convert.js
+++ convert.js
@@ -1,1 +1,85 @@
-console.log('Not implemented yet!');
+const temp = {};
+temp.operation;
+
+if (process.argv.length-2 != 4) {
+    printError();
+} else {
+    if (!!parseFloat(process.argv[2])) {
+        checkArgs(3);
+        if(process.argv[4] !== 'to'){
+            printError();
+        }
+        checkArgs(5);
+    } else {
+        printError();
+    }
+}
+
+convert();
+
+function printError(){
+    console.log('Invalid parameters');
+    process.exit(-1);
+}
+function checkArgs(index){
+    if(temp.operation){
+        if(temp.operation === -1 && process.argv[index].indexOf('g') !== -1){
+            return;
+        }
+        if(temp.operation === 1 && process.argv[index].indexOf('m') !== -1){
+            return;
+        }
+        printError();
+    }
+    switch(process.argv[index]){
+        case 'm':
+        case 'mm':
+        case 'cm':
+            temp.operation = 1;
+            break;
+        case 'kg':
+        case 'g':
+        temp.operation = -1;
+            break;
+        default:
+            printError();
+            break;
+    }
+}
+function convert(){
+    if(process.argv[3] === process.argv[5])
+        printResult(process.argv[2]);
+    if(temp.operation === -1){
+        if(process.argv[3] === 'kg'){
+            printResult(parseFloat(process.argv[2]) * 1000);
+        }else{
+            printResult(parseFloat(process.argv[2]) * 0.001);
+        }
+    }else{
+        if(process.argv[3] === 'm'){
+            if(process.argv[5] == 'mm'){
+                printResult(parseFloat(process.argv[2]) * 1000);
+            }else{
+                printResult(parseFloat(process.argv[2]) * 100);
+            }
+        }else{
+            if(process.argv[3] === 'mm'){
+                if(process.argv[5] === 'm'){
+                    printResult(parseFloat(process.argv[2]) * 0.001);
+                }else{
+                    printResult(parseFloat(process.argv[2]) * 0.01);
+                }
+            }else{
+                if(process.argv[5] === 'm'){
+                    printResult(parseFloat(process.argv[2]) * 0.01);
+                }else{
+                    printResult(parseFloat(process.argv[2]) * 10);
+                }
+            }
+        }
+    }
+}
+function printResult(a){
+    console.log(`${process.argv[2]} ${process.argv[3]} are ${a} ${process.argv[5]}`);
+    process.exit(-1);
+}
